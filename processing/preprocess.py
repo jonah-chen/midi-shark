4e7156118dc2c9253a20ba90bca465a02ccffbb1
Joe Hattori <joe2ninja21@gmail.com>
2021-10-20 12:10:24 -0400

    add comment

--- processing/preprocess.py
+++ processing/preprocess.py
@@ -6,17 +6,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def spectogram_librosa(wav_file_path):
+def spectogram_librosa(wav_file_path, show=False):
     y, sr = librosa.load(wav_file_path)
     y = librosa.feature.melspectrogram(
-        y=y,
-        sr=16000,
-        hop_length=512,
-        n_fft=2048,
-        n_mels=229)
+        y=y,  # audio time-series
+        sr=16000,  # sampling rate of y
+        hop_length=512,  # number of samples between successive frames.
+        n_fft=2048,  # length of the FFT window
+        n_mels=229,  # number of Mel bands to generate
+    )
     y = librosa.power_to_db(y, ref=np.max)
-    librosa.display.specshow(y, x_axis='time', y_axis='mel')
-    plt.show()
+    if show:
+        librosa.display.specshow(y, x_axis='time', y_axis='mel')
+        plt.show()
+    return y
 
 
 if __name__ == '__main__':
