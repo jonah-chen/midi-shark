3076eb50a39accde9a149fbd9ffbf0ef1682796a
khanatatac <khanatatac@gmail.com>
2021-11-01 14:07:38 -0400

    Finished transformer (Encoder+Decoder)

--- model/transformert.py
+++ model/transformert.py
@@ -66,7 +66,7 @@ class Encoder(nn.Module):
         dropout = 0.1, 
     ):
         super().__init__()
-        self.embedding = nn.Embedding(dim_in,dim_model)
+        #self.embedding = nn.Embedding(dim_in,dim_model)
         self.pe = PositionalEmbedding()
         self.layers = get_clones(EncoderLayer(dim_model, num_heads, dim_ff, dropout), num_layers)
 
@@ -77,17 +77,91 @@ class Encoder(nn.Module):
             x = self.layers[i](x)
         return x
 
-#Check
-src = torch.rand(64, 16, 512)
-src = torch.tensor(src)
-print(src.shape)
-tEncoder = Encoder(
-            dim_in = 3,
-            num_layers = 6,
-            dim_model = 512, 
-            num_heads = 8, 
-            dim_ff = 2048, 
-            dropout = 0.1, 
+class DecoderLayer(nn.Module):
+    def __init__(
+        self, 
+        dim_model = 512, 
+        num_heads = 6, 
+        dim_ff = 2048, 
+        dropout = 0.1, 
+    ):
+        super().__init__()
+        multihead = torch.nn.MultiheadAttention
+        self.attention1 = Residualdropout(
+            multihead(dim_model, num_heads, batch_first=True),
+            dimension=dim_model,
+            dropout=dropout,
         )
-out = tEncoder(src,6)
-print(out.shape)
+        self.attention2 = Residualdropout(
+            multihead(dim_model, num_heads, batch_first=True),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.feed_forward = Residualdropout(
+            FeedForward(dim_model, dim_ff),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+
+    def forward(self, tgt: Tensor, memory: Tensor) -> Tensor:
+        tgt = self.attention1(tgt, tgt, tgt)
+        tgt = self.attention2(memory, memory, tgt)
+        return self.feed_forward(tgt)
+
+class Decoder(nn.Module):
+    def __init__(
+        self, 
+        num_layers = 6,
+        dim_model = 512, 
+        num_heads = 8, 
+        dim_ff = 2048, 
+        dropout = 0.1, 
+    ):
+        super().__init__()
+        self.layers = get_clones(DecoderLayer(dim_model, num_heads, dim_ff, dropout), num_layers)
+        self.linear = nn.Linear(dim_model, dim_model)
+        self.pe = PositionalEmbedding()
+
+    def forward(self, tgt: Tensor, memory: Tensor, num_layers) -> Tensor:
+        tgt = self.pe(tgt)
+        for i in range(num_layers):
+            tgt = self.layers[i](tgt,memory)
+        return torch.softmax(self.linear(tgt), dim =1)
+
+class Transformer(nn.Module):
+    def __init__(
+        self, 
+        num_encoder_layers = 6,
+        num_decoder_layers = 6,
+        dim_model = 512, 
+        num_heads = 8, 
+        dim_ff = 2048, 
+        dropout: float = 0.1, 
+        activation: nn.Module = nn.ReLU(),
+    ):
+        super().__init__()
+        self.encoder = Encoder(
+            dim_in=3,
+            num_layers=num_encoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_ff=dim_ff,
+            dropout=dropout,
+        )
+        self.decoder = Decoder(
+            num_layers=num_decoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_ff=dim_ff,
+            dropout=dropout,
+        )
+
+    def forward(self, src: Tensor, tgt: Tensor) -> Tensor:
+        return self.decoder(tgt, self.encoder(src,6),6)
+
+src = torch.rand(64, 16, 512)
+tgt = torch.rand(64, 16, 512)
+res = Transformer()(src, tgt)
+print('input shape: ' + str(src.shape))
+print('output shape: ' + str(res.shape))
+print('same size: '+ str(res.shape==src.shape))
