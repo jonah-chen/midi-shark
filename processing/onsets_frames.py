46bc2fd7d73db4dfbb1f1ad244b548c13cae5f12
jonah-chen <jonah.chen@mail.utoronto.ca>
2021-11-27 01:14:44 -0500

    added save_offsets, and fixed crucial bug for frame

--- processing/onsets_frames.py
+++ processing/onsets_frames.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from dotenv import load_dotenv
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 
 def divide_notes(notes, folder_name, name, hop_length=20):
@@ -20,10 +21,6 @@ def divide_notes(notes, folder_name, name, hop_length=20):
         t_start = int(i)
         t_end = t_start + segment_length
         notes_spectrogram_final = notes[:, t_start:t_end]
-
-        if not os.path.isdir(folder_name):
-            os.makedirs(folder_name)
-
         output_name = os.path.join(
             folder_name, f"offset_{i*hop_length/1000}_duration_20")
         np.save(output_name, notes_spectrogram_final)
@@ -46,10 +43,29 @@ def save_onsets(notes, output_folder, hop_length=20):
     length = int(np.ceil(np.max(notes[:, 2])/hop_length))
     onsets = np.zeros((88, length), dtype=np.uint8)
     for note in notes:
-        index = round(note[1]/hop_length)
+        index = int(note[1])//hop_length
         onsets[int(note[0])-21, index] = 1
 
     divide_notes(onsets, output_folder, hop_length)
+
+
+def save_offsets(notes, output_folder, hop_length=20):
+    '''Generate the offset vectors
+
+    Args:
+        notes (np.array): Loaded notes file in the format of
+                          [note, start_time, end_time, velocity]
+        output_folder (str): Output folder name (i.e. "../data/offsets/2018")
+        hop_length (int, optional): Length of each bin in milliseconds.
+                                    Defaults to 20.
+    '''
+    length = int(np.ceil(np.max(notes[:, 2])/hop_length))
+    offsets = np.zeros((88, length), dtype=np.uint8)
+    for note in notes:
+        index = int(note[2])//hop_length
+        offsets[int(note[0])-21, index] = 1
+
+    divide_notes(offsets, output_folder, hop_length)
 
 
 def save_velocities(notes, output_folder, hop_length=20):
@@ -66,7 +82,7 @@ def save_velocities(notes, output_folder, hop_length=20):
     length = int(np.ceil(np.max(notes[:, 2])/hop_length))
     velocities = np.zeros((88, length), dtype=np.float32)
     for note in notes:
-        index = round(note[1]/hop_length)
+        index = int(note[1])//hop_length
         velocities[int(note[0])-21, index] = note[3]/127
 
     divide_notes(velocities, output_folder, hop_length)
@@ -83,18 +99,18 @@ def save_frames(notes, output_folder, hop_length=20):
     '''
 
     length = int(np.ceil(np.max(notes[:, 2])/hop_length))
-    frames = np.zeros((88, length))
+    frames = np.zeros((88, length), dtype=np.uint8)
 
     # Set a 1 in notes_spectrogram for each note between start and end time
     for i in notes:
         # get start and end time
-        start = int(i[1])
-        end = int(i[2])
+        start = int(i[1])//hop_length
+        end = int(i[2])//hop_length
         # get note
         note = int(i[0])-21
 
         # Update notes_spectrogram
-        frames[note, int(start):int(end)] = 1
+        frames[note, start:end] = 1
 
     divide_notes(frames, output_folder, hop_length)
 
@@ -104,8 +120,18 @@ if __name__ == '__main__':
     data_root = os.environ.get('dataname')
     notes_root = os.path.join(data_root, "notes_generated")
 
+    # generate directories. This is not thread safe so doing it first.
+    print("generating directories")
     for year in os.listdir(notes_root):
         for filename in os.listdir(os.path.join(notes_root, year)):
+            for data_type in ['onsets', 'offsets', 'velocities', 'frames']:
+                folder_name = os.path.join(
+                    data_root, data_type, year, filename)[:-4]
+                if not os.path.isdir(folder_name):
+                    os.makedirs(folder_name)
+
+    for year in os.listdir(notes_root):
+        for filename in tqdm(os.listdir(os.path.join(notes_root, year)), ascii=True, desc=str(year)):
             input_path = os.path.join(notes_root, year, filename)
             notes = np.load(input_path)
 
@@ -113,6 +139,8 @@ if __name__ == '__main__':
             with ThreadPoolExecutor() as executor:
                 executor.submit(save_onsets, notes, os.path.join(
                     data_root, "onsets", year, filename))
+                executor.submit(save_offsets, notes, os.path.join(
+                    data_root, "offsets", year, filename))
                 executor.submit(save_velocities, notes, os.path.join(
                     data_root, "velocities", year, filename))
                 executor.submit(save_frames, notes, os.path.join(
